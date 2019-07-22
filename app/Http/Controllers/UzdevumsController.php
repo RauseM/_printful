1793254b794d81e70807baf1981cabed5cdd0520
Marta <51164032+RauseM@users.noreply.github.com>
2019-07-22 20:44:37 +0300

    Add files via upload

---
+++ app/Http/Controllers/UzdevumsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Uzdevums;
+use Illuminate\Http\Request;
+
+class UzdevumsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Uzdevums  $uzdevums
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Uzdevums $uzdevums)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Uzdevums  $uzdevums
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Uzdevums $uzdevums)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Uzdevums  $uzdevums
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Uzdevums $uzdevums)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Uzdevums  $uzdevums
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Uzdevums $uzdevums)
+    {
+        //
+    }
+}
