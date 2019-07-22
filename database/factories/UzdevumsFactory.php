73c70cb036c55b20e8470fb2df98df4922d95b2c
Marta <51164032+RauseM@users.noreply.github.com>
2019-07-22 20:43:22 +0300

    Add files via upload

---
+++ database/factories/UzdevumsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Uzdevums;
+use Faker\Generator as Faker;
+
+$factory->define(Uzdevums::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
